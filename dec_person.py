4b9861d930cb98db3a9c31f30a629966af794026
Igor Marques <igormarques821@gmail.com>
2026-04-05 02:33:06 -0300

    Update dec_person.py

--- dec_person.py
+++ dec_person.py
@@ -1,1 +1,28 @@
-print("BORA LA")
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("yolov8n.pt")
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Erro ao abrir webcam")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Erro ao capturar frame")
+        break
+
+    results = model(frame)
+    annotated = results[0].plot()
+
+    cv2.imshow("YOLO", annotated)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
